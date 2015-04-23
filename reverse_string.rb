25b8141f6e4f1109e9bf2efebfcbf8918f0a7361
Travis Perdue <perduetravis@gmail.com>
2015-04-23 16:02:25 +0000

    reverse string problem

---
+++ reverse_string.rb
@@ -0,0 +1,16 @@
+# original string
+string = "This is a string in reverse"
+# change to array to manipulate
+array = string.split("")
+index = array.length
+# new array to save reverse version
+reverse_array = []
+
+#until loop iterating through array starting with last item in array working down to 0 and saving it to reverse_array
+until index == -1
+  reverse_array = reverse_array.prepend(array[index])
+  index -= 1
+end
+
+
+  
